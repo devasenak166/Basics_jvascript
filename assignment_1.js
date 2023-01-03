2c7fbc6c5162eb10d0235c9693c9b4257aa82e28
devasenak166 <121279794+devasenak166@users.noreply.github.com>
2023-01-03 22:45:33 +0530

    Add files via upload

---
+++ assignment_1.js
@@ -0,0 +1,30 @@
+var Name="Devasena"
+var age=22
+var salary=33333.50
+var working=false
+console.log(Name+" Name is declare as a string ")
+console.log(age+" Age is declare as a integer")
+console.log(salary+" salary is declare as a float")
+console.log(working+" work is declare as a boolean")
+
+// Using var and let and const 
+ var given=2
+ const pi=2.17
+ let value=2
+ var diameter=4
+
+function area(X1,X2){
+    return X1*X2*X2
+}
+function circumference(y1,y2,y3){
+      return y1*y2*y3
+
+}
+function radius(z1,z2)
+{
+    return z1/z2
+        
+}
+console.log(area(pi,given)+" Area of circle")
+console.log(circumference(value,pi,given)+" Circumference of a circle")
+console.log(radius(diameter,value)+" Radius of a circle if diameter given ")
